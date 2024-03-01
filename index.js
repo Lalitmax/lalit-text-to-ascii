c1986201dabcb1c172bbd716b2bb44de3b7692ef
Lalit Max <lalitmay@gmail.com>
2024-03-01 14:20:53 +0530

    change text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import figlet from "figlet";
 
 export const toAscii = (text) => {
 
-   return  figlet("Hello World!!", function (err, data) {
+   return  figlet(text, function (err, data) {
         if (err) {
             console.log("Something went wrong...");
             console.dir(err);
